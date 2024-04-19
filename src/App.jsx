6b679bcb42e035f46f41a593b1922578560d82fc
Pahari47 <nachiketapahari00@gmail.com>
2024-04-20 00:19:08 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './componenets/Navbar'
+import LandingPage from './componenets/LandingPage'
+
+function App() {
+  return (
+    <div className='w-full h-screen text-white'>
+      <Navbar />
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/componenets/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='w-full h-screen bg-zinc-900'>
+      <div className='textstructure'>
+        <div className="masker font-[Test Founders Grotesk X-Cond SmBd]">
+            <h1 className='uppercase'>We Create</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
